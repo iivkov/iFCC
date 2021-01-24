58e5c125b9819185639122143fe3f299fe045b51
Ivan <ivan.ivkovic1996@gmail.com>
2021-01-24 22:11:37 +0100

    Assignment

---
+++ scripts/app.js
@@ -0,0 +1,12 @@
+function cartHandler()
+{
+    var modal = document.getElementById("cartModal");
+    if (modal.style.display === "none") 
+    {
+      modal.style.display = "block";
+    } 
+    else 
+    {
+      modal.style.display = "none";
+    }
+}  
